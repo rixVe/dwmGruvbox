ee45c462eb8437434b8fa81b0d601fab265d4160
rixVe <piotr.litewka.2006@gmail.com>
2022-08-17 16:51:53 +0200

    Changed file manager to nemo and added pfetch configuration

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,14 @@ if status is-interactive
 end
 set fish_greeting
 alias v="nvim"
+
+export PF_COL1=3
+export PF_COL2=8
+export PF_COL3=7
+export PF_INFO="ascii title os host kernel wm uptime pkgs"
+export PF_ALIGN=8
+export PF_SEP=""
+
 pfetch
 function fish_mode_prompt
 end
